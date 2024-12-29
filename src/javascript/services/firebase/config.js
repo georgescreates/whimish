19290bb4d7a1d71cf965e613670f9113fabb3492
Georges Creates <georgescreates@gmail.com>
2024-12-28 21:41:13 -0500

    to hide firebase credentials

--- src/javascript/services/firebase/config.js
+++ src/javascript/services/firebase/config.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAgcj_utGKxLLATg1I6nNsipzmXbjyvIO4",
-    authDomain: "whimish-beings.firebaseapp.com",
-    projectId: "whimish-beings",
-    storageBucket: "whimish-beings.firebasestorage.app",
-    messagingSenderId: "498172548190",
-    appId: "1:498172548190:web:e4143f14c636c52db210cf",
-    measurementId: "G-HQXEM31GS3"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
